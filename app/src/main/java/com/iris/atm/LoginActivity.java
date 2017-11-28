aae76e04b051ef962b8d649e2edbf4245f8d9dc2
Lin Iris <iris861219@gmail.com>
2017-11-28 11:37:05 +0800

    login

--- app/src/main/java/com/iris/atm/LoginActivity.java
+++ app/src/main/java/com/iris/atm/LoginActivity.java
@@ -1,7 +1,12 @@
 package com.iris.atm;
 
+import android.os.StrictMode;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+    }
+    public void login(View view){
+        EditText edUserid = (EditText) findViewById(R.id.userid);
+        EditText edPasswd = (EditText) findViewById(R.id.passwd);
+        String userid = edUserid.getText().toString();
+        String passwd = edPasswd.getText().toString();
+        if("iris".equals(userid) && "1234".equals(passwd)){
+            Toast.makeText(this,"登入成功",Toast.LENGTH_LONG).show();
+            finish();
+        }else {
+            new AlertDialog.Builder(this)
+                    .setMessage("登入失敗" )
+                    .setTitle("Atm")
+                    .setPositiveButton("Ok",null)
+                    .show();
+        }
+    }
+    public void cancel(View view){
+
     }
 }

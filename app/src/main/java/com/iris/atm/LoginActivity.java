ad244d447b4f71be1baf50f8dca030894d861ca2
liniris <iris861219@gmail.com>
2017-12-05 12:05:26 +0800

    onActivityResult

--- app/src/main/java/com/iris/atm/LoginActivity.java
+++ app/src/main/java/com/iris/atm/LoginActivity.java
@@ -22,6 +22,9 @@ public class LoginActivity extends AppCompatActivity {
         String passwd = edPasswd.getText().toString();
         if("iris".equals(userid) && "1234".equals(passwd)){
             Toast.makeText(this,"登入成功",Toast.LENGTH_LONG).show();
+            getIntent().putExtra("LOGIN_USERID", userid);
+            getIntent().putExtra("LOGIN_PASSWD", passwd);
+            setResult(RESULT_OK, getIntent());
             finish();
         }else {
             new AlertDialog.Builder(this)
--- app/src/main/java/com/iris/atm/MainActivity.java
+++ app/src/main/java/com/iris/atm/MainActivity.java
@@ -6,12 +6,29 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     boolean logon = false;
+    public static final int FUNC_LOGIN = 1;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == FUNC_LOGIN){
+            if (resultCode == RESULT_OK){
+                String userid = data.getStringExtra("LOGIN_USERID");
+                String passwd = data.getStringExtra("LOGIN_PASSWD");
+                Log.d("Result", userid + "/" + passwd);
+            }else{
+                finish();
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +46,8 @@ public class MainActivity extends AppCompatActivity {
         });
         if (!logon) {
             Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
+//            startActivity(intent);
+        startActivityForResult(intent, FUNC_LOGIN);
         }
     }
 

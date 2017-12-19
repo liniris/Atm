9e700a154b280b422b7c3e44b3030606cf1a7790
liniris <iris861219@gmail.com>
2017-12-19 11:19:16 +0800

    spinner

---
+++ app/src/main/java/com/iris/atm/Ch8Activity.java
@@ -0,0 +1,13 @@
+package com.iris.atm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ch8Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ch8);
+    }
+}
